8c735068897d43aa0204132033bd2956d0bc3c9d
Ashutosh <ashu.ydv2001@gmail.com>
2023-11-27 23:59:11 +0530

    updated

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
                         <div className="flex flex-wrap w-full bg-[#242424] py-32 px-10 relative mb-4 rounded-3xl overflow-hidden">
                             <div className="text-center relative z-10 w-full">
                                 <h2 className="text-4xl text-white font-medium title-font mb-2">Explore Blockchain Resources</h2>
-                                <p class="leading-relaxed text-gray-200">Discover a curated list of blockchain resources, from the basics to roadmap generators, and more.</p>
+                                <p className="leading-relaxed text-gray-200">Discover a curated list of blockchain resources, from the basics to roadmap generators, and more.</p>
                             </div>
                             <div className={`absolute -top-8 -left-8 rounded-full bg-orange-400 w-20 h-20`}></div>
                             <div className={`absolute -top-8 -right-8 rounded-full bg-orange-400 w-20 h-20`}></div>
@@ -23,7 +23,7 @@ const Hero = () => {
                                 <div className="flex flex-wrap w-full bg-[#242424] sm:py-24 py-16 sm:px-10 px-6 relative rounded-3xl overflow-hidden">
                                     <div className="text-center relative z-10 w-full">
                                         <h2 className="text-2xl text-white font-medium title-font mb-2">Blockchain Projects Showcase</h2>
-                                        <p class="leading-relaxed text-gray-200">Explore a curated list of blockchain projects, ranging from foundational concepts to advanced developments.</p>
+                                        <p className="leading-relaxed text-gray-200">Explore a curated list of blockchain projects, ranging from foundational concepts to advanced developments.</p>
                                     </div>
                                     <div className={`absolute -top-10 -right-10 rounded-full bg-orange-400 w-20 h-20`}></div>
                                     <div className={`absolute -bottom-8 -left-8 rounded-full border-8 border-orange-400 w-20 h-20`}></div>
@@ -35,7 +35,7 @@ const Hero = () => {
                                 <div className="flex flex-wrap w-full bg-[#242424] sm:py-24 py-16 sm:px-10 px-6 relative rounded-3xl overflow-hidden">
                                     <div className="text-center relative z-10 w-full">
                                         <h2 className="text-2xl text-white font-medium title-font mb-2">Contribute to Open Source Repositories</h2>
-                                        <p class="leading-relaxed text-gray-200">Get involved in open-source projects with our curated list of blockchain repositories, perfect for contributors of all levels.</p>
+                                        <p className="leading-relaxed text-gray-200">Get involved in open-source projects with our curated list of blockchain repositories, perfect for contributors of all levels.</p>
                                     </div>
                                     <div className={`absolute -top-10 -left-10 rounded-full bg-orange-400 w-20 h-20`}></div>
                                     <div className={`absolute -bottom-8 -right-8 rounded-full border-8 border-orange-400 w-20 h-20`}></div>
